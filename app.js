2323cad5f78ad9451576a5f61fb82059c9b88c13
sideeq12 <abdwaheed2018@gmail.com>
2020-11-20 10:07:10 -0800

    Api available

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.urlencoded({extended : true}))
 
-mongoose.connect("mongodb+srv://sideeq:@cluster0.7rhnk.mongodb.net/myapiDB", {useNewUrlParser : true, useUnifiedTopology : true})
+mongoose.connect("mongodb+srv://sideeq:"+process.env.PASS+"@cluster0.7rhnk.mongodb.net/MyAPI", {useNewUrlParser : true, useUnifiedTopology : true})
 
 const myApischema = mongoose.Schema({
     title : String,
@@ -18,6 +19,30 @@ const MyPoem = mongoose.model("MyPoem", myApischema)
 
 app.get("/", (req, res)=>{
    res.sendFile( __dirname +"/index.html")
+})
+app.post("/", (req, res)=>{
+    let newPoem = new MyPoem({
+        title : req.body.title,
+        Poem : req.body.poem,
+        Author : req.body.author
+    })
+    newPoem.save((err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.sendFile(__dirname+"/success.html")
+        }
+    })
+  
+})
+app.get("/request", (req,res)=>{
+    MyPoem.findOne({ _id : "5fb802863b16901d48c188cc"}, (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+          res.send(result)
+        }
+    })
 })
 
 app.listen(8008, ()=>{
